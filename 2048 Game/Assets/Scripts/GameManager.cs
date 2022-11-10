2222536e200ad28f512b55797bba33f15a4975dc
Dandelikeswood <j.boudouresque@gmail.com>
2022-11-10 18:41:49 +0100

    Background Ajustement

--- 2048 Game/Assets/Scripts/GameManager.cs
+++ 2048 Game/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int _width = 4;
     [SerializeField] private int _height = 4;
     [SerializeField] private Node _nodePrefab;
+    [SerializeField] private SpriteRenderer _boardPrefab;
 
     private void Start()
     {
@@ -22,5 +23,12 @@ public class GameManager : MonoBehaviour
                 var node = Instantiate(_nodePrefab, new Vector2(x, y), Quaternion.identity);
             }
         }
+
+        var center = new Vector2((float)_width /2 - 0.5f,(float) _height / 2 -0.5f);
+
+        var board = Instantiate(_boardPrefab, center, Quaternion.identity);
+        board.size = new Vector2(_width, _height);
+
+        Camera.main.transform.position = new Vector3(center.x, center.y, -10);
     }
 }
